a569e551584b3476698a9436e84dbde277d6de7d
Anny27-pixel <jabintasmin102523@gmail.com>
2022-08-18 18:51:54 +0600

    set allert

--- js/withdraw.js
+++ js/withdraw.js
@@ -17,24 +17,39 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
     const newWithdrawAmmountString = withdrawField.value;
     const newWithdrawAmmount = parseFloat(newWithdrawAmmountString);
 
+    // step-7:
+    withdrawField.value = '';
+
+    if (isNaN(newWithdrawAmmount)) {
+        alert('Please provide a valid number');
+        return;
+    }
     // step-3:
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawTotalString = withdrawTotalElement.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
 
 
-    // step-4:
-    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmmount;
-    withdrawTotalElement.innerText = currentWithdrawTotal;
+
 
     // step-5:
     const balanceTotalElement = document.getElementById('balance-total');
     const previousBalanceTotalString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
 
+
+
+    if (newWithdrawAmmount > previousBalanceTotal) {
+        alert('Baap er bank e eto taka nai');
+        return;
+    }
+    // step-4:
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
+
     // step-6:
     const newBalanceTotal = previousBalanceTotal - newWithdrawAmmount;
     balanceTotalElement.innerText = newBalanceTotal;
-    // step-7:
-    withdrawField.value = '';
+
+
 })
